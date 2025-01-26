fd7c8673efa202e51fad30d7d7b803b2c30e6e59
adityaexe <kmradi2002@gmail.com>
2025-01-26 18:16:21 +0530

    Add AllHero component to render Title and Hero1 components

---
+++ src/components/AllHero.jsx
@@ -0,0 +1,12 @@
+import Hero1 from "../All-Hero-Section/Hero-1/Hero1";
+import Title from "./Title";
+
+const AllHero = () => {
+  return (
+    <>
+      <Title title={"All Hero"} /> <Hero1 />
+    </>
+  );
+};
+
+export default AllHero;
