d40cbc4157fc1e581d735efb9d5f5c7913d455a4
adityaexe <kmradi2002@gmail.com>
2025-01-27 15:54:39 +0530

    Update Navbar component to enhance title styling with larger font size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 w-full bg-gradient-to-r from-pink-500 via-gray-500 to-gray-800 shadow-lg z-50">
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="text-white font-bold text-xl tracking-wide">
-          FunkyNav
+          <p className="text-3xl">FunkyNav</p>
         </div>
         {/* Desktop Menu */}
         <ul className="hidden sm:flex space-x-6 text-white">
