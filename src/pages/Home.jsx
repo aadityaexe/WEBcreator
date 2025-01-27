6d62cc184fdc175168dbae798d1799b40d18c5d1
adityaexe <kmradi2002@gmail.com>
2025-01-27 16:17:57 +0530

    Add MeemTokenWebsites component to display themed website options on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,34 @@ import AllRoadmap from "../components/AllRoadmap";
 import AllToknomics from "../components/AllToknomics";
 import ContactUs from "../components/ContactUs";
 import Hero from "../components/Hero";
+import MeemTokenWebsites from "../components/MeemTokenWebsites";
 import Services from "../components/Services";
-
+import wifu from "../assets/image.png";
 const Home = () => {
+  const websites = [
+    {
+      domain: "wifuwiki.netlify.app",
+      theme: "Funky Pink",
+      image: wifu,
+    },
+    {
+      domain: "wifuwiki.netlify.app",
+      theme: "Gray Gradient",
+      image: wifu,
+    },
+    {
+      domain: "wifuwiki.netlify.app",
+      theme: "Minimalist Dark",
+      image: wifu,
+    },
+  ];
+
   return (
     <div>
       <Hero />
       <AboutUs />
+      <MeemTokenWebsites websites={websites} />
+
       <AllHero />
       <AllRoadmap />
       <AllToknomics />
