8ba8fb14a0b3f896874b436bc62354b5bede6eef
adityaexe <kmradi2002@gmail.com>
2025-01-27 09:26:35 +0530

    Add AboutUs, Services, AllRoadmap, AllToknomics, and ContactUs components to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import AboutUs from "../components/AboutUs";
 import AllHero from "../components/AllHero";
+import AllRoadmap from "../components/AllRoadmap";
+import AllToknomics from "../components/AllToknomics";
+import ContactUs from "../components/ContactUs";
 import Hero from "../components/Hero";
+import Services from "../components/Services";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <AllHero />
+      <AllRoadmap />
+      <AllToknomics />
+      <AboutUs />
+      <Services />
+      <ContactUs />
     </div>
   );
 };
