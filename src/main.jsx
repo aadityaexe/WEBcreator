9cf90acbf0c48d39877af092469a3f75ec247efb
adityaexe <kmradi2002@gmail.com>
2025-01-29 14:49:15 +0530

    Implement routing with createBrowserRouter and add AllTypeOfHeros page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import AllTypeOfHeros from "./pages/AllTypeOfHeros.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <App />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <Home />
+          </>
+        ),
+      },
+      {
+        path: "/all-heros",
+        element: (
+          <>
+            <AllTypeOfHeros />
+          </>
+        ),
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
