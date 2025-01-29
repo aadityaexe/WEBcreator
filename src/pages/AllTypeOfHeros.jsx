09d30d88a22ce050c0d2c590a49c7f508707ae08
adityaexe <kmradi2002@gmail.com>
2025-01-29 14:49:28 +0530

    Add AllTypeOfHeros component to display Hero1

---
+++ src/pages/AllTypeOfHeros.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero1 from "../All-Hero-Section/Hero-1/Hero1";
+const AllTypeOfHeros = () => {
+  return (
+    <div className="h-screen">
+      <Hero1 />
+    </div>
+  );
+};
+
+export default AllTypeOfHeros;
