64631dca539161f942d8673f6ec1f506dbadf98f
Aditya Kashyap <adityakashyap7998@gmail.com>
2025-01-29 21:18:18 +0530

    hero added in diff files

--- src/pages/AllTypeOfHeros.jsx
+++ src/pages/AllTypeOfHeros.jsx
@@ -1,10 +1,10 @@
 import Title from "../components/Title";
 import Hero1 from "../All-Hero-Section/Hero-1/Hero1";
-import Hero2 from '../All-Hero-Section/Hero-1/Hero2';
-import Hero3 from "../All-Hero-Section/Hero-1/Hero3";
-import Hero4 from "../All-Hero-Section/Hero-1/Hero4";
-import Hero5 from "../All-Hero-Section/Hero-1/Hero5";
-import Hero6 from "../All-Hero-Section/Hero-1/Hero6";
+import Hero2 from '../All-Hero-Section/Hero-2/Hero2'
+import Hero3 from "../All-Hero-Section/Hero-3/Hero3";
+import Hero4 from "../All-Hero-Section/Hero-4/Hero4";
+import Hero5 from "../All-Hero-Section/Hero-5/Hero5";
+import Hero6 from "../All-Hero-Section/Hero-6/Hero6";
 
 const AllTypeOfHeros = () => {
   return (
