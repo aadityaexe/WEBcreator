ab436a22391a4158f5516e641ae93f7ad6b6ef55
adityaexe <kmradi2002@gmail.com>
2025-02-01 14:30:44 +0530

    Display selected components in SeeCreatedWebsite page with safe cloning

--- src/pages/SeeCreatedWebsite.jsx
+++ src/pages/SeeCreatedWebsite.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useContent } from "../Store/ContentValues";
 
 const SeeCreatedWebsite = () => {
-  return <div>SeeCreatedWebsite</div>;
+  const { selectedComponents } = useContent();
+
+  return (
+    <div className="h-screen pt-28">
+      {selectedComponents.map((component, index) => (
+        <div key={index}>
+          <h3>{component.content.type.name}</h3>
+          {/* Safely clone the component, ensuring the ref is passed correctly */}
+          {React.cloneElement(component, {
+            ...component.props,
+            ref: component.ref || null, // Ensuring ref is not invalid
+          })}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SeeCreatedWebsite;
