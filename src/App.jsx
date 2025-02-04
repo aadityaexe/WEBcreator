7189f9a0d38088998da305816637d4ca4acb6e8d
adityaexe <kmradi2002@gmail.com>
2025-02-04 20:04:07 +0530

    Enhance App component layout with main wrapper for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-r from-pink-500 via-gray-800 to-gray-900">
+    <div className="min-h-screen flex flex-col bg-gradient-to-r from-pink-500 via-gray-800 to-gray-900">
       <Navbar />
-      <Outlet />
+      <main className="flex-grow p-4">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
