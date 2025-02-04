4f917c68df376f7428ce2bff2d532a6047cb8fb8
adityaexe <kmradi2002@gmail.com>
2025-02-04 19:41:44 +0530

    Refactor main.jsx to integrate ContentProvider and streamline routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,79 +7,25 @@ import AllTypeOfHeros from "./pages/AllTypeOfHeros.jsx";
 import AllTypeOfRoadmap from "./pages/AllTypeOfRoadmap.jsx";
 import CreateWebsite from "./pages/CreateWebsite.jsx";
 import EnterWebsiteInfo from "./pages/EnterWebsiteInfo.jsx";
-import { ContentProvider } from "./Store/ContentValues.jsx";
 import SeeCreatedWebsite from "./pages/SeeCreatedWebsite.jsx";
 import GetWebsiteCode from "./pages/GetWebsiteCode.jsx";
+import { ContentProvider } from "./Store/ContentValues.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <>
-        <ContentProvider>
-          <App />
-        </ContentProvider>
-      </>
-    ),
+    element: <App />,
     children: [
-      {
-        path: "/",
-        element: (
-          <>
-            <Home />
-          </>
-        ),
-      },
-      {
-        path: "/all-heros",
-        element: (
-          <>
-            <AllTypeOfHeros />
-          </>
-        ),
-      },
-      {
-        path: "/all-roadmap",
-        element: (
-          <>
-            <AllTypeOfRoadmap />
-          </>
-        ),
-      },
-      {
-        path: "/new-website",
-        element: (
-          <>
-            <CreateWebsite />
-          </>
-        ),
-      },
-      {
-        path: "/enter-website-info",
-        element: (
-          <>
-            <EnterWebsiteInfo />
-          </>
-        ),
-      },
-      {
-        path: "/see-created-website",
-        element: (
-          <>
-            <SeeCreatedWebsite />
-          </>
-        ),
-      },
-      {
-        path: "/get-website-code",
-        element: (
-          <>
-            <GetWebsiteCode />
-          </>
-        ),
-      },
+      { path: "/", element: <Home /> },
+      { path: "/all-heros", element: <AllTypeOfHeros /> },
+      { path: "/all-roadmap", element: <AllTypeOfRoadmap /> },
+      { path: "/new-website", element: <CreateWebsite /> },
+      { path: "/enter-website-info", element: <EnterWebsiteInfo /> },
+      { path: "/see-created-website", element: <SeeCreatedWebsite /> },
+      { path: "/get-website-code", element: <GetWebsiteCode /> },
     ],
   },
 ]);
+
 createRoot(document.getElementById("root")).render(
   <ContentProvider>
     <RouterProvider router={router} />
