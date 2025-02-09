847eb32109ff4b93391e71cb0e89a08d443f7eee
adityaexe <kmradi2002@gmail.com>
2025-02-09 14:51:31 +0530

    Refactor AllTypeOfRoadmap component structure and update layout semantics

--- src/pages/AllTypeOfRoadmap.jsx
+++ src/pages/AllTypeOfRoadmap.jsx
@@ -4,18 +4,21 @@ import Title from "../components/Title";
 
 const AllTypeOfHeros = () => {
   return (
-    <div className="min-h-screen px-4 md:px-8 lg:px-16 pt-10 space-y-16">
-      <div className="max-w-7xl mx-auto space-y-16">
+    <section className="min-h-screen px-4 md:px-8 lg:px-16 py-10 space-y-16">
+      <div className="max-w-7xl mx-auto flex flex-col space-y-16">
+        {/* Roadmap 1 Section */}
         <div>
           <Title title="Roadmap 1" />
           <Roadmap1 />
         </div>
+
+        {/* Roadmap 2 Section */}
         <div>
           <Title title="Roadmap 2" />
           <Roadmap2 />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
