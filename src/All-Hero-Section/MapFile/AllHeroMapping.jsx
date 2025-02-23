103f510395f587f83cbf4146040cadc5108b0a65
adityaexe <kmradi2002@gmail.com>
2025-02-23 20:42:34 +0530

    Add additional Hero components to AllHeroMapping

--- src/All-Hero-Section/MapFile/AllHeroMapping.jsx
+++ src/All-Hero-Section/MapFile/AllHeroMapping.jsx
@@ -1,21 +1,39 @@
-import Hero1 from "../Hero-1/Hero1"
-import Hero2 from '../Hero-2/Hero2';
+import Hero1 from "../Hero-1/Hero1";
+import Hero2 from "../Hero-2/Hero2";
 import Hero3 from "../Hero-3/Hero3";
 import Hero4 from "../Hero-4/Hero4";
 import Hero5 from "../Hero-5/Hero5";
 import Hero6 from "../Hero-6/Hero6";
+import Hero7 from "../Hero-7/Hero7";
+import Hero8 from "../Hero-8/Hero8";
+import Hero9 from "../Hero-9/Hero9";
+import Hero10 from "../Hero-10/Hero10";
+import Hero11 from "../Hero-11/Hero11";
+import Hero12 from "../Hero-12/Hero12";
+import Hero13 from "../Hero-13/Hero13";
+import Hero14 from "../Hero-14/Hero14";
+import Hero15 from "../Hero-15/Hero15";
+import Hero16 from "../Hero-16/Hero16";
+import Hero17 from "../Hero-17/Hero17";
 
+const components = [
+  { id: "hero-1", component: Hero1 },
+  { id: "hero-2", component: Hero2 },
+  { id: "hero-3", component: Hero3 },
+  { id: "hero-4", component: Hero4 },
+  { id: "hero-5", component: Hero5 },
+  { id: "hero-6", component: Hero6 },
+  { id: "hero-7", component: Hero7 },
+  { id: "hero-8", component: Hero8 },
+  { id: "hero-9", component: Hero9 },
+  { id: "hero-10", component: Hero10 },
+  { id: "hero-11", component: Hero11 },
+  { id: "hero-12", component: Hero12 },
+  { id: "hero-13", component: Hero13 },
+  { id: "hero-14", component: Hero14 },
+  { id: "hero-15", component: Hero15 },
+  { id: "hero-16", component: Hero16 },
+  { id: "hero-17", component: Hero17 },
+];
 
-
-const components = [ 
-    { id: "hero-1", component: Hero1 },
-    { id: "hero-2", component: Hero2 },
-    { id: "hero-3", component: Hero3 },
-    { id: "hero-4", component: Hero4 },
-    { id: "hero-5", component: Hero5 },
-    { id: "hero-6", component: Hero6 },
-  ];
-  
-  export default components;
-  
- 
+export default components;
