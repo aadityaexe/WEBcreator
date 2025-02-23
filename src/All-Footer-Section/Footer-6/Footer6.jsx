f1f20f5b9c853d4804adc2b939cbe8b35424cb13
adityaexe <kmradi2002@gmail.com>
2025-02-23 20:48:25 +0530

    Refactor Footer6 component for improved readability and formatting

--- src/All-Footer-Section/Footer-6/Footer6.jsx
+++ src/All-Footer-Section/Footer-6/Footer6.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer6 = () => {
   return (
@@ -7,13 +6,23 @@ const Footer6 = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <img src="/path-to-your-logo.png" alt="Logo" className="h-10" />
-          <p className="text-lg">&copy; 2025 Your Company. All Rights Reserved.</p>
+          <p className="text-lg">
+            &copy; 2025 Your Company. All Rights Reserved.
+          </p>
         </div>
         <div className="flex space-x-6">
-          <a href="#" className="text-white hover:text-blue-400"><FaFacebook size={24} /></a>
-          <a href="#" className="text-white hover:text-blue-400"><FaTwitter size={24} /></a>
-          <a href="#" className="text-white hover:text-blue-400"><FaInstagram size={24} /></a>
-          <a href="#" className="text-white hover:text-blue-400"><FaLinkedin size={24} /></a>
+          <a href="#" className="text-white hover:text-blue-400">
+            <FaFacebook size={24} />
+          </a>
+          <a href="#" className="text-white hover:text-blue-400">
+            <FaTwitter size={24} />
+          </a>
+          <a href="#" className="text-white hover:text-blue-400">
+            <FaInstagram size={24} />
+          </a>
+          <a href="#" className="text-white hover:text-blue-400">
+            <FaLinkedin size={24} />
+          </a>
         </div>
       </div>
     </footer>
