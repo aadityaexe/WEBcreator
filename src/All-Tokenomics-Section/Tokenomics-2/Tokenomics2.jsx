540c21abcc6fb5110a8ede3b0d406f00290c60b7
adityaexe <kmradi2002@gmail.com>
2025-02-25 21:09:26 +0530

    Refactor Token2 component for improved readability and formatting

--- src/All-Tokenomics-Section/Tokenomics-2/Tokenomics2.jsx
+++ src/All-Tokenomics-Section/Tokenomics-2/Tokenomics2.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Token2 = () => {
   return (
     <section className="py-20 bg-gray-200">
@@ -9,28 +7,40 @@ const Token2 = () => {
           <div className="flex justify-between">
             <span className="text-lg text-gray-600">Initial Supply</span>
             <div className="w-1/2 bg-gray-400 h-6 rounded-full">
-              <div className="bg-blue-500 h-6 rounded-full" style={{ width: '40%' }}></div>
+              <div
+                className="bg-blue-500 h-6 rounded-full"
+                style={{ width: "40%" }}
+              ></div>
             </div>
             <span className="text-lg text-gray-600">40%</span>
           </div>
           <div className="flex justify-between">
             <span className="text-lg text-gray-600">Staking Rewards</span>
             <div className="w-1/2 bg-gray-400 h-6 rounded-full">
-              <div className="bg-green-500 h-6 rounded-full" style={{ width: '30%' }}></div>
+              <div
+                className="bg-green-500 h-6 rounded-full"
+                style={{ width: "30%" }}
+              ></div>
             </div>
             <span className="text-lg text-gray-600">30%</span>
           </div>
           <div className="flex justify-between">
             <span className="text-lg text-gray-600">Liquidity Pool</span>
             <div className="w-1/2 bg-gray-400 h-6 rounded-full">
-              <div className="bg-yellow-500 h-6 rounded-full" style={{ width: '20%' }}></div>
+              <div
+                className="bg-yellow-500 h-6 rounded-full"
+                style={{ width: "20%" }}
+              ></div>
             </div>
             <span className="text-lg text-gray-600">20%</span>
           </div>
           <div className="flex justify-between">
             <span className="text-lg text-gray-600">Development</span>
             <div className="w-1/2 bg-gray-400 h-6 rounded-full">
-              <div className="bg-red-500 h-6 rounded-full" style={{ width: '10%' }}></div>
+              <div
+                className="bg-red-500 h-6 rounded-full"
+                style={{ width: "10%" }}
+              ></div>
             </div>
             <span className="text-lg text-gray-600">10%</span>
           </div>
